df2b0fdeaddcda74567e28103bec0698f1b2ba0c
Ivan Druzhinin <dia.doca@gmail.com>
2021-11-28 17:58:30 +0300

    gitignore
    Ignore unwanted files
    
    main feature added
    
    loading label
    
    comment changed
    
    i = 1

--- Repo3/ViewController.swift
+++ Repo3/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var i = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // LOGO!
     }
 
 
 }
 
+extension  ViewController {
+
+    var mainFeature: Bool { true }
+
+}
